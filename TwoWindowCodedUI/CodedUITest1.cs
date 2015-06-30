dcc70f6a38499ad84ffa888a8f6b00026fecf318
Prateek Bhuwania <prateekchinu3011@gmail.com>
2015-06-30 17:57:07 +0530

    dfsdf

--- TwoWindowCodedUI/CodedUITest1.cs
+++ TwoWindowCodedUI/CodedUITest1.cs
@@ -10,7 +10,7 @@ using Microsoft.VisualStudio.TestTools.UITest.Extension;
 using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
 
 
-namespace TwoWindowCodedUI
+namspace TwoWindowCodedUI
 {
     /// <summary>
     /// Summary description for CodedUITest1
